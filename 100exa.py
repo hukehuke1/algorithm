a3692d3453e4c99bf1198ec585c81304be7de210
exHooker <hukehuke@sina.com>
2018-04-24 17:42:12 +0800

    100exa update

--- 100exa.py
+++ 100exa.py
@@ -10,6 +10,8 @@
                    2018/4/16:
 -------------------------------------------------
 """
+import random
+import time
 
 def exa1():
     for i in range(1,5):
@@ -49,7 +51,83 @@ def exa4():  #输入某年某月某日，判断这一天是这一年的第几天
     print("今天是第%s天"%sum)
 
 def exa5():
+    l = []
+    for i in range(1,4):
+        x = input("请输入数字")
+        l.append(int(x))
+    l.sort()    
+    print(l)
+
+def exa6(n):
+    a ,b = 1 ,1
+    for i in range(n-1):
+        a,b = b,a+b
+    return a
+
+def exa6b(n):  #递归
+    if n ==1 or n == 2:
+        return 1
+    else :
+        return exa6b(n-1)+exa6b(n-2)
+
+def exa7():
+    a = [1,2,3]
+    b = a[:]
+    print(b)
+
+def exa8():
+    for i in range(1,10):
+        print()
+        for j in range(1,i):
+            print("%s*%s=%s"%(j,i,str(i*j)),end = ' ')
+
+
+def exa9():
+    for i in range(10):
+        print(i)
+        time.sleep(1)
+        print(time.time())
+        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+
+
+def exa11(n):
+    a ,b = 1,1
+    for i in range(n-1):
+        a ,b = b, a+b
+        print(a)
+
+from math import sqrt
+
+def exa12():#判断101-200之间有多少个素数，并输出所有素数。
+    leap = 1
+    h = 0
+    for i in range(101,201):
+        sqt = int(sqrt(i+1))
+
+        for j in range(2,sqt+1):
+            if i % j == 0:
+                leap = 0
+                break
+        if leap == 1:
+            print('%s是素数'%i)
+            h += 1
+        leap = 1
+    print('一共有%s个数'%h)
+
+
+def exa13(): #打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身
+    for n in range(100,1000):
+        i = int(n / 100)
+        j = int((n % 100)/10)
+        k = int((n % 10))
+        if n == i**3 + j**3 + k**3:
+            print("%s是水仙花数"%n)
+
+
+def exa14(a): #分解质因数
     pass
 
 if __name__ =='__main__':
-    exa4()
+    #print(int(random.uniform(1,211)))
+    exa14(100)
